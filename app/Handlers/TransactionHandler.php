b27b14b3eacd2286a9ffa07ec83d2d55af899dca
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-11-04 22:53:57 +0600

    added observer and transaction model

---
+++ app/Handlers/TransactionHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Handlers;
+
+
+use App\Models\Transaction;
+use Carbon\Carbon;
+
+class TransactionHandler
+{
+    public function createTransaction($isExpense, $amount, Carbon $dateTime, $relatedModel, $remark){
+        $newTransaction = new Transaction();
+        $newTransaction->model_name = get_class($relatedModel);
+        $newTransaction->model_id = $relatedModel->id;
+        $newTransaction->type = $isExpense == 1;
+        $newTransaction->amount = $amount;
+        $newTransaction->time = $dateTime;
+        $newTransaction->remark = $remark;
+        $newTransaction->save();
+    }
+}
--- app/Observers/BookingObserver.php
+++ app/Observers/BookingObserver.php
@@ -4,7 +4,7 @@ namespace App\Observers;
 
 use App\Models\Booking;
 
-class UserObserver
+class BookingObserver
 {
     /**
      * Handle the booking "created" event.
