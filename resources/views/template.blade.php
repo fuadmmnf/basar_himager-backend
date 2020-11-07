bcefba6db0ebd8505824894723ec05d8caa2c8a7
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-11-07 19:28:18 +0600

    template receipt/report added

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -66,7 +66,6 @@
 
 </div>
 <span align="center" style="line-height: 1.2;">
-{{--    <p style="font-size: 1.4rem; font-weight: bold">Salary Report</p>--}}
     <p><b>Report No:</b> 03edkd</p>
     <p><b>Date:</b> {{ date('F d, Y') }}</p>
 </span>
