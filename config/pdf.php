85f1856f99abdd041c6058ad334c0f599513cf25
MANNAN <bsse0920@iit.du.ac.bd>
2021-01-18 23:16:42 +0600

    fixed bangla font on report

--- config/pdf.php
+++ config/pdf.php
@@ -13,10 +13,10 @@ return [
     'font_data'             => [
 
         'kalpurush' => [
-            'R'  => 'Kalpurush-Regular.ttf',    // regular font
-            'B'  => 'Kalpurush-Regular.ttf',       // optional: bold font
-            'I'  => 'Kalpurush-Regular.ttf',     // optional: italic font
-            'BI' => 'Kalpurush-Regular.ttf', // optional: bold-italic font
+            'R'  => 'Kalpurush.ttf',    // regular font
+            'B'  => 'Kalpurush.ttf',       // optional: bold font
+            'I'  => 'Kalpurush.ttf',     // optional: italic font
+            'BI' => 'Kalpurush.ttf', // optional: bold-italic font
             'useOTL' => 0xFF,    // required for complicated langs like Persian, Arabic and Chinese
             'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
         ],
