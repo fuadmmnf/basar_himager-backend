45c26221687dfc9c1df072fba9a49ff9ce3f0833
MANNAN <bsse0920@iit.du.ac.bd>
2021-01-18 22:39:56 +0600

    added booking money income as separate category

--- app/Observers/BookingObserver.php
+++ app/Observers/BookingObserver.php
@@ -21,7 +21,7 @@ class BookingObserver
                 $booking, 'New Booking'
             );
         }
-        else if ($booking->booking_amount > 0) {
+        if ($booking->booking_amount > 0) {
             $transactionHandler = new TransactionHandler();
             $transactionHandler->createTransaction(0, $booking->booking_amount, $booking->booking_time,
                 $booking, 'Booking Money'
