94cd90de9811d1ec572eeeb4167b09eae4a3dada
MANNAN <bsse0920@iit.du.ac.bd>
2021-01-19 13:40:32 +0600

    checking store potato receipt

--- resources/views/store_potato_receipt.blade.php
+++ resources/views/store_potato_receipt.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <style>
         body {
-            font-family: 'Kalpurush', 'AdorshoLipi', sans-serif;
+            font-family: 'AdorshoLipi', 'Kalpurush',  sans-serif;
         }
 
         table {
@@ -15,7 +15,7 @@
 
         th, td {
             padding: 7px;
-            font-family: 'Kalpurush', 'AdorshoLipi', sans-serif;
+            font-family: 'AdorshoLipi',  'Kalpurush', sans-serif;
             font-size: 15px;
         }
 
