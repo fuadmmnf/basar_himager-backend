fd9273d5d6efe39f1e9344c7232e662c796e7df0
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-08-05 21:53:44 +0600

    fixed fetch of deliverygroups in deliveries table

--- database/migrations/2022_08_05_213004_add_type_to_deliverygroups.php
+++ database/migrations/2022_08_05_213004_add_type_to_deliverygroups.php
@@ -1,12 +1,13 @@
 <?php
+use Illuminate\Database\Migrations\Migration;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class {
+class AddTypeToDeliveryGroups extends Migration{
 	public function up() {
 		Schema::table('deliverygroups', function (Blueprint $table) {
-            $table->integer('type')->default(0); // 0 => contains delivery, 1 => only loan collection
+            $table->integer('type')->default(0)->after('delivery_time'); // 0 => contains delivery, 1 => only loan collection
 		});
 	}
 
