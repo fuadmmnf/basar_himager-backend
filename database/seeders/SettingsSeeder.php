b3e9e8735c2a8d98c5e876e437756cf27a65e6eb
fuadmmnf <bsse0920@iit.du.ac.bd>
2022-08-28 11:34:47 +0600

    changed default service charge rate in seeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -10,13 +10,18 @@ class SettingsSeeder extends \Illuminate\Database\Seeder
 {
     public function run()
     {
-        $keys = ['do_charge', 'fancost_per_bag', 'service_charge_rate'];
+        $keys = ['do_charge', 'fancost_per_bag'];
         foreach ($keys as $key){
             settings::create([
                 'key' => $key,
                 'value' => 5,
             ]);
         }
+        settings::create([
+            'key' => 'service_charge_rate',
+            'value' => 18,
+        ]);
+
         settings::create([
             'key' => 'current_sr_no',
             'value' => 0,
